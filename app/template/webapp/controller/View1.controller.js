03003b8348aadacdf345aebcbd92cce928c1aa5d
Artem Chymbur <artemchimbur@gmail.com>
2024-08-23 14:30:10 +0000

    Added json model

--- app/template/webapp/controller/View1.controller.js
+++ app/template/webapp/controller/View1.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("ui.template.controller.View1", {
         onInit: function () {
-
+            var oTreeModel = new JSONModel({
+                headerTitle : "Mosec"
+    
+            });
+            this.getView().setModel(oTreeModel, "treeModel");
         }
     });
 });
