3fffbad6bf8d7b3e1fd320ccce638be154901e9e
Artem Chymbur <artemchimbur@gmail.com>
2024-08-26 14:12:12 +0000

    added changes

--- app/template/webapp/controller/View1.controller.js
+++ app/template/webapp/controller/View1.controller.js
@@ -8,7 +8,15 @@ function (Controller, JSONModel) {
     return Controller.extend("ui.template.controller.View1", {
         onInit: function () {
             var oTreeModel = new JSONModel({
-                headerTitle : "Mosec"
+                headerTitle : "HR template",
+                headerSubTitle : "HR dashboard template from Mosec Solutions",
+                headerActDate : "Date: 26.08.2024 17:37:59",
+                headerCurrUser : "User: Steve Otieno",
+                headerIconSrc : "templateapp\img\mosec_logo.png",
+                footerCopyright : "Copyright© 2024",
+                footerClass : "Templates",
+                footerPO : "Steve Otieno",
+                footerDetails : "Dashboard details"
     
             });
             this.getView().setModel(oTreeModel, "treeModel");
