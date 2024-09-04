d812b90529b9f775ac2488608cfd072c00db4954
Artem Chymbur <artemchimbur@gmail.com>
2024-09-04 13:55:06 +0000

    Added Dashboard tab

---
+++ app/template/webapp/controller/Report.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller'
+    
+], function(Controller) {
+"use strict";
+
+var Controller = Controller.extend("ui.template.controller.Table", {
+    
+    onInit : function (oTemplateController) {
+        
+       
+    }
+});
+
+return Controller;
+
+});
